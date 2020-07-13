39b087258b7218371e534f1e25a32aa4c9219da0
Byron <byron19pv@gmail.com>
2020-07-13 17:09:12 -0500

    Finalizing the code

---
+++ app/src/main/java/com/platzi/conf/model/Speaker.kt
@@ -0,0 +1,16 @@
+package com.platzi.conf.model
+
+import java.io.Serializable
+import java.util.*
+
+class Speaker (): Serializable{
+
+
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+
+}
